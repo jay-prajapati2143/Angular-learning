0b22e802d253951a61d0485072113ce5b9e1b69f
gsolanki <gsolanki@westcoastuniversity.edu>
2023-06-25 21:27:04 +0530

    Databinding in angular

--- my-fisrt-app/src/app/server/server.component.ts
+++ my-fisrt-app/src/app/server/server.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector : 'app-server',
+    selector : 'app-server', // this is a Element Selector
+    //selector : '[app-server]', // This is a Attribute Selector
+
+    /* Attribute Selector :
+    We can use Attribute as selector to use or render the 
+    component content as well as Element by declare it in []  */
+
+    //selector : '.app-server', // This is a Class Selector.
+
+    /* Class Selector :
+    We can also use Class as selector to use or render the 
+    component content as well as Element by declare it with '.'  */
+    
+
     templateUrl : './server.component.html'
     
 })
 export class ServerComponent{
-
+  serverId:number = 10;
+  serverStatus:string = 'offline';
 }
--- my-fisrt-app/src/app/servers/servers.component.ts
+++ my-fisrt-app/src/app/servers/servers.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  userName = 'DefaultUser'
+  ifUserName = false;
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true; 
+    }, 2000);
+  }
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  }
+  OnUpdateServerName(event: Event){
+      console.log(event);
+      this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  ResetUser(){
+    this.userName = '';
+  }
 }
